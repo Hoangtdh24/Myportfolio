a63c533196b907fa267117faea07f784fd4d932c
Hoangtdh24 <hoangtdh24@gmail.com>
2024-08-17 13:20:06 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from PIL import Image
+
+def txt(a):  # 1 column - paragraph
+    _, col, _ = st.columns((1,3,1))
+    with col:
+        st.markdown(a)
+
+def txt1(a, b):  # 2 columns with offset to the left (2:1)
+  _, col1, col2, _ = st.columns([1,2,1,1])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(b)
+
+def txt2(a, b):  # 2 columns with offset to the right (1:2)
+  _, col1, col2, _ = st.columns([1,1,2,1])
+  with col1:
+    st.markdown(f'`{a}`')  # highlight text
+  with col2:
+    st.markdown(b)
+
+def txt3(a, b):  # 2 columns with offset to the right (1:2)
+  _, col1, col2, _ = st.columns([1,1,2,1])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(b)
+  
+def txt4(a, b, c): # 3 columns
+  _, col1, col2, _, col3, _ = st.columns([1,0.65,1.5, 0.1,0.75,1])
+  with col1:
+    st.markdown(f'`{a}`')  # highlight text
+  with col2:
+    st.markdown(b)
+  with col3:
+    st.markdown(c)
+
+st.set_page_config(page_title='Le Nguyen Gia Bao - Portfolio',page_icon="☀", layout="wide")
+
+with open("style.css") as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+image = Image.open('./image/your_image.png')
+_, img_column, _ = st.columns((2,1,2))
+img_column.image(image, use_column_width  = True)
+
+st.write('''
+# YOUR NAME
+##### A Developer from Vietnam 🇻🇳  
+''')
