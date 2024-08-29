5b734920374a89c18e3d35e2cc984b1d86dac63a
Hoangtdh24 <hoangtdh24@gmail.com>
2024-08-29 23:42:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,10 +31,11 @@ with inf_linkedin:
 with inf_address:
     st.info('Hà Đông, Hà Nội',icon = ":material/location_on:")
 
-st.header('## MỤC TIÊU NGHỀ NGHIỆP',divider ='gray')
+st.header('MỤC TIÊU NGHỀ NGHIỆP',divider ='gray')
 st.markdown('Trở thành chuyên gia về phân tích dữ liệu và khoa học dữ liệu')
-st.header('## HỌC VẤN',divider ='gray')
+st.header('HỌC VẤN',divider ='gray')
 w_hocvan("Đại học chính quy","Đại học Ngoại thương","Kinh tế đối ngoại","Xuất sắc","2016-2020")
+w_hocvan("THPT","THPT Chuyên Thái Bình","Chuyên Toán","Giải nhì VMO 2016","2013-2016")
 
 
 
